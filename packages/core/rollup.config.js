bee53a7b1f947861cf674cfb6faab8365fdc6a54
Sergei.Mozhaev <mozhaev.dev@gmail.com>
2024-12-18 15:23:37 +0200

    add rollup

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from 'rollup-plugin-typescript2'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    json(),
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
+  external: [],
+}
